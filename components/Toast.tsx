854b1233f5d1dad91de4302a7bcc2e7f12157f2b
Malik Hisyam <hisyamabdulmalikcsgo@gmail.com>
2024-04-22 02:35:06 +0700

    Animation and better styling is updated

---
+++ components/Toast.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Toast = () => {
+    return (
+        <motion.div
+            className='fixed bottom-10 bg-green-500 p-4 rounded-xl'
+            initial={{ opacity: 0, right: -1000 }}
+            animate={{ opacity: 1, right: 40 }}
+            transition={{ duration: 0.3 }}
+            exit={{ opacity: 0, right: -1000 }}
+        >
+            <h1 className='text-white text-sm font-bold'>Successfully created a data</h1>
+        </motion.div>
+    )
+}
+
+export default Toast
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Space, Row, Col, Divider, Input, Button, Card, Modal } from 'antd';
+import { Space, Row, Col, Divider, Input, Button, Card } from 'antd';
 import { WithDefaultLayout } from '../components/DefautLayout';
 import { Page } from '../types/Page';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
@@ -6,6 +6,8 @@ import { CreateCustomerSchemaType } from '@/schemas/CreateCustomerSchema';
 import { useAtom } from 'jotai';
 import { lineAtom } from '@/store/line';
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion'
+import Toast from '@/components/Toast';
 
 const IndexPage: Page = () => {
     const [line, setLine] = useAtom(lineAtom);
@@ -19,6 +21,11 @@ const IndexPage: Page = () => {
         if (!updatedLine[randLine]) {
             updatedLine[randLine] = [];
         }
+
+        if (!data.name) {
+            setUniqueError("Name can't be empty");
+            return;
+        }
         // Name uniqueness validation
         const findName = updatedLine.some(line => line.includes(data.name));
 
@@ -26,11 +33,17 @@ const IndexPage: Page = () => {
             setUniqueError('No duplicate names are allowed');
             return;
         }
+        console.log(data.name)
+
 
         updatedLine[randLine] = [...(updatedLine[randLine] || []), data.name];
         setLine(updatedLine);
         setUniqueError(null);
         setIsModalOpen(true);
+
+        setTimeout(() => {
+            setIsModalOpen(false);
+        }, 3000);
     }
 
 
@@ -45,36 +58,72 @@ const IndexPage: Page = () => {
     return (
         <>
             <Divider orientation='left'>Line Queue Viewer</Divider>
-            <Row className='p-4 bg-slate-200 rounded-xl shadow-xl' gutter={[0, 32]}>
-                <Col span={24}>
-                    <Row gutter={32} >
+            <Row className='p-4 bg-slate-900 rounded-xl shadow-xl h-5/6' justify={'center'} align={'bottom'} gutter={[0, 32]}>
+                <Col className='self-start' span={24}>
+                    <Row gutter={24} >
                         <Col span={8} >
-                            <h1 className='text-center font-bold text-xl'>Cashier 1</h1>
-                            <Space direction='vertical' style={{ width: '100%' }}>
-                                {line[0] && line[0].slice(0, 3).map((item, index) => (
-                                    <Card className='shadow-md' key={index}><p className='text-center'>{item}</p></Card>
-                                ))}
+                            <h1 className='text-center font-bold text-xl text-white'>Cashier 1</h1>
+                            <Space direction='vertical' style={{ width: '100%', gap: '1.5rem' }}>
+                                {line[0] && line[0].slice(0, 3).map((item, index) => {
+                                    return (
+                                        <AnimatePresence key={index}>
+                                            <motion.div
+                                                initial={{ scale: 0, opacity: 0 }}
+                                                animate={{ scale: 1, opacity: 1 }}
+                                                transition={{ duration: 0.5 }}
+                                                exit={{ scale: 0, opacity: 0 }}
+                                            >
+                                                <Card className='shadow-md' ><p className='text-center'>{item}</p></Card>
+                                            </motion.div>
+
+                                        </AnimatePresence>
+
+                                    )
+                                })}
                                 {line[0] && line[0].length > 3 && <Card className='shadow-md'><p className='text-center '>{line[0].length - 3} more</p></Card>}
                             </Space>
                         </Col>
                         <Col span={8}>
                             <div className=''>
-                                <h1 className='text-center font-bold text-xl'>Cashier 2</h1>
-                                <Space className='' direction='vertical' style={{ width: '100%' }}>
-                                    {line[1] && line[1].slice(0, 3).map((item, index) => (
-                                        <Card className='shadow-md' key={index}><p className='text-center'>{item}</p></Card>
-                                    ))}
+                                <h1 className='text-center font-bold text-xl text-white'>Cashier 2</h1>
+                                <Space className='' direction='vertical' style={{ width: '100%', gap: '1.5rem' }}>
+                                    {line[1] && line[1].slice(0, 3).map((item, index) => {
+                                        return (
+                                            <AnimatePresence key={index}>
+                                                <motion.div
+                                                    initial={{ scale: 0, opacity: 0 }}
+                                                    animate={{ scale: 1, opacity: 1 }}
+                                                    transition={{ duration: 0.5 }}
+                                                    exit={{ scale: 0, opacity: 0 }}
+                                                >
+                                                    <Card className='shadow-md' ><p className='text-center'>{item}</p></Card>
+                                                </motion.div>
+                                            </AnimatePresence>
+                                        )
+                                    })}
                                     {line[1] && line[1].length > 3 && <Card className='shadow-md'><p className='text-center'>{line[1].length - 3} more</p></Card>}
                                 </Space>
                             </div>
                         </Col>
                         <Col span={8}>
                             <div className=''>
-                                <h1 className='text-center font-bold text-xl'>Cashier 3</h1>
-                                <Space direction='vertical' style={{ width: '100%' }}>
-                                    {line[2] && line[2].slice(0, 3).map((item, index) => (
-                                        <Card className='shadow-md' key={index}><p className='text-center'>{item}</p></Card>
-                                    ))}
+                                <h1 className='text-center font-bold text-xl text-white'>Cashier 3</h1>
+                                <Space direction='vertical' style={{ width: '100%', gap: '1.5rem' }}>
+                                    {line[2] && line[2].slice(0, 3).map((item, index) => {
+                                        return (
+                                            <AnimatePresence key={index}>
+                                                <motion.div
+                                                    initial={{ scale: 0, opacity: 0 }}
+                                                    animate={{ scale: 1, opacity: 1 }}
+                                                    transition={{ duration: 0.5 }}
+                                                    exit={{ scale: 0, opacity: 0 }}
+                                                >
+                                                    <Card className='shadow-md' ><p className='text-center'>{item}</p></Card>
+                                                </motion.div>
+                                            </AnimatePresence>
+
+                                        )
+                                    })}
                                     {line[2] && line[2].length > 3 && <Card className='shadow-md'><p className='text-center'>{line[2].length - 3} more</p></Card>}
                                 </Space>
                             </div>
@@ -82,41 +131,45 @@ const IndexPage: Page = () => {
                     </Row >
                 </Col >
                 <Col span={24}>
-                    <Row className='rounded-xl' gutter={8} justify={'center'}>
+                    <Row className='rounded-xl'>
                         <Col span={12}>
-                            <form onSubmit={handleSubmit(onSubmit)}>
+                            <form className='flex flex-col gap-2' onSubmit={handleSubmit(onSubmit)}>
                                 <Controller
                                     name='name'
                                     control={control}
                                     render={({ field }) => (
-                                        <Input {...field} type='text' />
+                                        <Input {...field} className='shadow-md' size='large' type='text' />
                                     )}
                                 />
                                 {uniqueError && <p className='text-red-400 font-bold'>{uniqueError}</p>}
-                                <Button htmlType='submit' className='w-1/2 bg-green-400' type='default'>Enter Line</Button>
+                                <Button htmlType='submit' className='w-4/6 bg-green-400 shadow-md text-white font-bold' size='large' type='default'>Enter Line</Button>
                             </form>
                         </Col>
-                        <Col span={12}>
-                            <Space className='flex' direction='vertical'>
-                                <Button onClick={() => handleCashier(0)} className='w-full bg-red-200'>Handle Cashier #1</Button>
-                                <Button onClick={() => handleCashier(1)} className='w-full bg-red-200'>Handle Cashier #2</Button>
-                                <Button onClick={() => handleCashier(2)} className='w-full bg-red-200'>Handle Cashier #3</Button>
-                            </Space>
+                        <Col span={12} >
+                            <div className='w-full flex flex-col justify-center items-center gap-2'>
+                                <Button type='default' onClick={() => handleCashier(0)} size='large' className='rounded-lg w-5/6 bg-red-400 font-bold text-white shadow-md'>Handle Cashier #1</Button>
+                                <Button type='default' onClick={() => handleCashier(1)} size='large' className='rounded-lg w-5/6 bg-red-400 font-bold text-white shadow-md'>Handle Cashier #2</Button>
+                                <Button type='default' onClick={() => handleCashier(2)} size='large' className='rounded-lg w-5/6 bg-red-400 font-bold text-white shadow-md'>Handle Cashier #3</Button>
+
+                            </div>
                         </Col>
                     </Row>
                 </Col>
             </Row >
-            <Modal
-                open={isModalOpen}
-                onCancel={() => setIsModalOpen(false)}
-                title={(uniqueError ? <h1 className='text-red-400'>Failed</h1> : <h1 className='text-green-400'>Success</h1>)}
-                footer={[
-                    <Button className='hover:bg-blue-400 hover:text-white' key="ok" onClick={() => setIsModalOpen(false)}>Ok</Button>
-                ]}
-            >
-                {uniqueError && <p>{uniqueError}</p>}
-                {!uniqueError && <p>Successfully created a data</p>}
-            </Modal >
+            {/* <Modal
+                    open={isModalOpen}
+                    onCancel={() => setIsModalOpen(false)}
+                    title={(uniqueError ? <h1 className='text-red-400'>Failed</h1> : <h1 className='text-green-400'>Success</h1>)}
+                    footer={[
+                        <Button className='hover:bg-blue-400 hover:text-white' key="ok" onClick={() => setIsModalOpen(false)}>Ok</Button>
+                    ]}
+                >
+                    {uniqueError && <p>{uniqueError}</p>}
+                    {!uniqueError && <p>Successfully created a data</p>}
+                </Modal > */}
+            <AnimatePresence>
+                {isModalOpen && <Toast />}
+            </AnimatePresence>
         </>
     );
 }
